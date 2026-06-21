d872013cf6ffa6522ed3baa8de857fb15f4a6c40
krishna chintakayala <43456492+kktejas07@users.noreply.github.com>
2026-06-21 07:35:08 -0400

    fix: add SessionProvider to wrap useSession hook

--- src/components/ui/providers.tsx
+++ src/components/ui/providers.tsx
@@ -1,13 +1,16 @@
 "use client";
 
+import { SessionProvider } from "next-auth/react";
 import { ThemeProvider } from "next-themes";
 import { Toaster } from "sonner";
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <ThemeProvider attribute="class" defaultTheme="dark" enableSystem={false}>
-      {children}
-      <Toaster richColors />
-    </ThemeProvider>
+    <SessionProvider>
+      <ThemeProvider attribute="class" defaultTheme="dark" enableSystem={false}>
+        {children}
+        <Toaster richColors />
+      </ThemeProvider>
+    </SessionProvider>
   );
 }
